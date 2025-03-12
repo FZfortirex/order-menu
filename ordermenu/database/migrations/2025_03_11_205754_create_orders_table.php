1197cfcc513c7db7ba82f0aa24e89026b997bfde
Zain <zainburhanuddin12@gmail.com>
2025-03-12 07:12:41 +0700

    Feat [BZ] Added Database

---
+++ ordermenu/database/migrations/2025_03_11_205754_create_orders_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id'); 
+            $table->integer('chat_id'); 
+            $table->integer('redeem_point_id'); 
+            $table->integer('table'); 
+            $table->string('additional_note');
+            $table->integer('total_price');
+            $table->string('status');
+            $table->integer('total_point');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
