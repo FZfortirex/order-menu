7c908c83a64b9599011336b12373200a1e988151
hilz06 <167834314+msbosss123@users.noreply.github.com>
2025-07-16 08:40:20 +0700

    add foto

--- ordermenu/resources/views/user/galeri-desktop.blade.php
+++ ordermenu/resources/views/user/galeri-desktop.blade.php
@@ -22,6 +22,10 @@
             <img src="{{ asset('images/foto-tempat2.png') }}" alt="Tempat 2" class="w-full aspect-square object-cover rounded shadow">
             <img src="{{ asset('images/foto-tempat3.png') }}" alt="Tempat 3" class="w-full aspect-square object-cover rounded shadow">
             <img src="{{ asset('images/foto-tempat4.png') }}" alt="Tempat 4" class="w-full aspect-square object-cover rounded shadow">
+            <img src="{{ asset('images/kmsawah.jpg') }}" alt="Tempat 5" class="w-full aspect-square object-cover rounded shadow">
+            <img src="{{ asset('images/kmsawah2.jpeg') }}" alt="Tempat 6" class="w-full aspect-square object-cover rounded shadow">
+            <img src="{{ asset('images/kmsawah3.jpeg') }}" alt="Tempat 7" class="w-full aspect-square object-cover rounded shadow">
+            <img src="{{ asset('images/kmsawah4.jpg') }}" alt="Tempat 8" class="w-full aspect-square object-cover rounded shadow">
         </div>
 
 
